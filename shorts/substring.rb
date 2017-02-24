15b00110f66052b375e6d4695378c38481ee5b65
Maya Novarini <Maya@maya-novarini-3.attlocal.net>
2017-02-23 22:38:35 -0800

    substring

---
+++ shorts/substring.rb
@@ -0,0 +1,10 @@
+=begin
+Write a method that returns a list of all substrings of a string that start at the beginning of the original string.
+The return value should be arranged in order from shortest to longest substring.
+=end
+
+def substrings_at_start(saywhat)
+  saywhat.size <= 1 ? [str] : [substrings_at_start(saywhat.chop), str].flatten
+end
+
+#Recursively chopping the last character off and the flatten method turns the multidimensional array into one dimensional array. 
