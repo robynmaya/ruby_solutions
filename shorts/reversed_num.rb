43b91ca112f74387975e173130e4e033b9a108d7
Maya Novarini <Maya@maya-novarini-3.attlocal.net>
2017-03-01 01:41:19 -0800

    reversed num

--- shorts/reversed_num.rb
+++ shorts/reversed_num.rb
@@ -1,12 +1,15 @@
 # Convert number to reversed array of digits
 
 def reversed_num(number)
-   number.to_s.chars.find_all{ |x| x.to_i > 0 }.reverse.map(&:to_i)
+  lam = lambda { |x| x.to_i > 0 }
+  number.to_s.chars.find_all(&lam).reverse.map(&:to_i).join.to_i
 end
 
 #turn the number into a string
 #turn the string into an array of characters
-#find_all takes out only number that is greater than 0
+#find_all through the lambda, takes out only number that is greater than 0
 #find_all returns an array
 #the new array is then reversed
-#finally -using map- we turn every string in the reversed array into an integer
+#using map, we turn every string in the reversed array into an integer
+#turn the array into a string
+#then at last, turn the string into integers...... god dang.
