27d64e2322c595437b458372d4ff786ab1842910
Maya Novarini <Maya@maya-novarini-3.attlocal.net>
2017-03-01 00:49:04 -0800

    double consonants

--- shorts/repeater.rb
+++ shorts/repeater.rb
@@ -5,4 +5,15 @@ def repeater(string)
 end
 
 #break down the string into an array of characters, zip it with the same data,
-#then combine the content and turn back the array into a string. 
+#then combine the content and turn back the array into a string.
+
+#####
+
+#Write a method that takes a string, and returns a new string in which every consonant character is doubled.
+#Vowels (a,e,i,o,u), digits, punctuation, and whitespace should not be doubled.
+
+def double_consonants(string)
+  p = lambda { |char| char.match(/[^aeiou\W\d]/i) ? char + char : char }
+
+  string.chars.map(&p).join
+end
