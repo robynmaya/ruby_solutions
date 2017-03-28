9893b8d3c6072e2c83f90697a289580822cfaa20
Maya Novarini <Maya@maya-novarini-3.attlocal.net>
2017-03-27 21:09:15 -0700

    add palindrome substrings

--- shorts/substring.rb
+++ shorts/substring.rb
@@ -1,27 +1,15 @@
 =begin
 Write a method that returns a list of all substrings of a string.
-The returned list should be ordered by where in the string the substring begins.
-This means that all substrings that start at position 0 should come first,
-then all substrings that start at position 1, and so on.
-Since multiple substrings will occur at each position,
-the substrings at a given position should be returned in order from shortest to longest.
 =end
 
-def subs(arr2, base=[])
-  1.upto(arr2.size).with_object(base) { |x, final_arr| final_arr << arr2.take(x) }.flatten!
-end
-
-def subs2(arr)
-  final_arr = []
-  arr.size.times do |i|
-    arr2 = arr.drop(i)
-    subs(arr2, final_arr)
-  end
-  final_arr
+def subs(str, base=[]) #method 1
+  arr2 = str.chars
+  1.upto(arr2.size).with_object(base) { |x, final_arr| final_arr << arr2.take(x) }.map(&:join)
 end
 
 =begin
-substrings('abcde') == [
+For the next method the output is below:
+subs2('abcde') == [
   'a', 'ab', 'abc', 'abcd', 'abcde',
   'b', 'bc', 'bcd', 'bcde',
   'c', 'cd', 'cde',
@@ -29,3 +17,19 @@ substrings('abcde') == [
   'e'
 ]
 =end
+
+def subs2(str, final_arr=[])
+  arr = str.chars
+  arr.size.times do |i|
+    str = arr.drop(i).join
+    subs(str, final_arr) #use the previous method 1
+  end
+  final_arr.map(&:join)
+end
+
+#find palindromes among the substrings
+def sub3_palindrome(string, result=[])
+  substrings = subs2(string) #use the previous method 2
+  palindrome = lambda { |substring| substring == substring.reverse if substring.length > 1 }
+  (result << substrings.select(&palindrome)).flatten
+end
