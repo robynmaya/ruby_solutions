8a23458425cbb8026129d757a741c438bb11dbee
Maya Novarini <Maya@maya-novarini-3.attlocal.net>
2017-07-19 20:45:25 -0700

    fib by length

---
+++ shorts/fibonacci_length.rb
@@ -0,0 +1,20 @@
+=begin
+The Fibonacci series is a series of numbers (1, 1, 2, 3, 5, 8, 13, 21, ...) such that the first 2 numbers are 1 by definition, and each subsequent number is the sum of the two previous numbers. This series appears throughout the natural world.
+
+Computationally, the Fibonacci series is a very simple series, but the results grow at an incredibly rapid rate. For example, the 100th Fibonacci number is 354,224,848,179,261,915,075 -- that's enormous, especially considering that it takes 6 iterations before it generates the first 2 digit number.
+
+Write a method that calculates and returns the index of the first Fibonacci number that has the number of digits specified as an argument. (The first Fibonacci number has index 1.)
+
+=end
+
+def find_fibonacci_index_by_length(len, index=1)
+  fib = ->(x){ x < 2 ? x : fib[x-1] + fib[x-2] }
+
+  loop do
+    break if fib.call(index).to_s.length >= len
+    index +=1
+  end
+
+  index
+
+end
