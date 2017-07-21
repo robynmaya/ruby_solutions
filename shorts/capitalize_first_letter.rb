2f047a0eb7dbc1aba59d8b2e3327833657176464
Maya Novarini <Maya@maya-novarini-3.attlocal.net>
2017-07-21 01:36:40 -0700

    capitalize first letter

---
+++ shorts/capitalize_first_letter.rb
@@ -0,0 +1,14 @@
+=begin
+Write a method that takes a single String argument and returns a new string
+that contains the original value of the argument with the first character
+of every word capitalized and all other letters lowercase.
+
+You may assume that words are any sequence of non-blank characters.
+=end
+
+
+def word_cap(string)
+  string.split.map{|i| i[0].upcase!+i[1..-1]}
+end
+
+word_cap('four score and seven') == ["Four", "Score", "And", "Seven"] #true
