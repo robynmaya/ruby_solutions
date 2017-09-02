3bc46290f9c300eb850762a5ff58743a1a57a3a6
thegirlfromipanema <mayanovarini@gmail.com>
2017-09-01 17:33:16 -0700

    guessing game

---
+++ shorts/guessing_game.rb
@@ -0,0 +1,79 @@
+# Create an object-oriented number guessing class for numbers in the range 1 to 100,
+# with a limit of 7 guesses per game. 
+
+class GuessingGame
+  MAX_GUESSES = 7
+  RANGE = 1..100
+
+  RESULT_OF_GUESS_MESSAGE = {
+    high:  "Your number is too high.",
+    low:   "Your number is too low.",
+    match: "That's the number!"
+  }.freeze
+
+  WIN_OR_LOSE = {
+    high:  :lose,
+    low:   :lose,
+    match: :win
+  }.freeze
+
+  RESULT_OF_GAME_MESSAGE = {
+    win:  "You won!",
+    lose: "You have no more guesses. You lost!"
+  }.freeze
+
+  def initialize
+    @secret_number = nil
+  end
+
+  def play
+    reset
+    game_result = play_game
+    display_game_end_message(game_result)
+  end
+
+  private
+
+  def reset
+    @secret_number = rand(RANGE)
+  end
+
+  def play_game
+    result = nil
+    MAX_GUESSES.downto(1) do |remaining_guesses|
+      display_guesses_remaining(remaining_guesses)
+      result = check_guess(obtain_one_guess)
+      puts RESULT_OF_GUESS_MESSAGE[result]
+      break if result == :match
+    end
+    WIN_OR_LOSE[result]
+  end
+
+  def display_guesses_remaining(remaining)
+    puts
+    if remaining == 1
+      puts 'You have 1 guess remaining.'
+    else
+      puts "You have #{remaining} guesses remaining."
+    end
+  end
+
+  def obtain_one_guess
+    loop do
+      print "Enter a number between #{RANGE.first} and #{RANGE.last}: "
+      guess = gets.chomp.to_i
+      return guess if RANGE.cover?(guess)
+      print "Invalid guess. "
+    end
+  end
+
+  def check_guess(guess_value)
+    return :match if guess_value == @secret_number
+    return :low if guess_value < @secret_number
+    :high
+  end
+
+  def display_game_end_message(result)
+    puts "", RESULT_OF_GAME_MESSAGE[result]
+  end
+end
